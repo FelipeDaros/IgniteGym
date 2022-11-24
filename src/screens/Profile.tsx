ed51aa9048f722ec15deeab06a41d3e01498892f
Felipe <felipe-daros@hotmail.com>
2022-11-24 07:55:07 -0300

    .

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,22 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, VStack } from "native-base";
 
 
 
 export function Profile(){
   return(
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil"/>
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto 
+            source={{uri: "https://github.com/felipedaros.png"}}
+            alt="Foto de perfil"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
